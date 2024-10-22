45ae2e8c57fd595d3134f40259d31ea9ba3848ea
Henrique <henriqueferreira0320@gmail.com>
2024-10-22 12:00:32 -0300

    Services Section;

--- index.php
+++ index.php
@@ -3,6 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Portfólio de um Desenvolvedor Front End e Web/UX Designer que busca proporcionar a melhor e mais bonita experiência ao usuário.">
+    <meta name="keywords" content="quinho, quinho pereira, quinho pereira portfolio, UX Designer, Desenvolvedor FrontEnd">
+    <meta name="robots" content="index, follow">
+    <meta property="og:title" content="Quinho Pereira" />
+    <meta property="og:description" content="Portfólio de Desenvolvedor FrontEnd e UX/Web Designer" />
+    <meta property="og:image" content="assets/og-image.jpg" />
+    <meta property="og:url" content="http://quinhopereira.com" />
+    <meta property="og:type" content="website" />
+    <link rel="canonical" href="http://quinhopereira.com">
     <?php include "includes/assets-head.php" ?>
     <title>Quinho Pereira</title>
 </head>
@@ -16,18 +25,18 @@
             <div class="dynamic-menu-wrapper__menu">
                 <ul class="anchor-list">
                     <li class="anchor-list__item">
-                        <a href="" class="anchor">Sobre mim</a>
+                        <a href="#about-me" class="anchor">Sobre mim</a>
                     </li>
                     <li class="anchor-list__item"> 
-                        <a href="" class="anchor">Cases</a>
+                        <a href="#my-cases" class="anchor">Cases</a>
                     </li>
                     <li class="anchor-list__item">
-                        <a class="anchor">Serviços</a>
+                        <a href="#services" class="anchor">Serviços</a>
                     </li>
                 </ul>
             </div>
             <div class="dynamic-menu-wrapper__button-trigger">
-                <button id="slide-down-menu" class="dynamic-menu-trigger-button">
+                <button id="slide-down-menu" class="dynamic-menu-trigger-button" aria-label="Abrir Menu">
                     <div class="line-top"></div>
                     <div class="svg-chevron-wrapper">
                         <svg width="25.16" xmlns="http://www.w3.org/2000/svg" height="23" id="screenshot-fbbc58aa-1f9c-8016-8005-20cc8895840f" viewBox="0 0 25.16 23" xmlns:xlink="http://www.w3.org/1999/xlink" fill="none" version="1.1"><g id="shape-fbbc58aa-1f9c-8016-8005-20cc8895840f" data-testid="Crevron para baixo" width="800px" height="800px" rx="0" ry="0" style="fill: rgb(0, 0, 0);"><g id="shape-fbbc58aa-1f9c-8016-8005-20cc88958410" data-testid="base-background" style="display: none;"><g class="fills" id="fills-fbbc58aa-1f9c-8016-8005-20cc88958410"><rect width="25.159802306425263" height="23" x="0" transform="matrix(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000)" style="fill: none;" ry="0" fill="none" rx="0" y="0"/></g></g><g id="shape-fbbc58aa-1f9c-8016-8005-20cc88958411" data-testid="SVGRepo_bgCarrier" rx="0" ry="0" style="fill: rgb(0, 0, 0);"/><g id="shape-fbbc58aa-1f9c-8016-8005-20cc88958412" data-testid="SVGRepo_tracerCarrier" rx="0" ry="0" style="fill: rgb(0, 0, 0);"/><g id="shape-fbbc58aa-1f9c-8016-8005-20cc88958413" data-testid="SVGRepo_iconCarrier" rx="0" ry="0" style="fill: rgb(0, 0, 0);"><g id="shape-fbbc58aa-1f9c-8016-8005-20cc88958414" data-testid="svg-path"><g class="fills" id="fills-fbbc58aa-1f9c-8016-8005-20cc88958414"><path d="M3.594,7.229L12.939,15.771L22.284,7.229M12.939,7.229L12.939,7.229Z" fill="none" stroke-linejoin="round" style="fill: none;"/></g><g fill="none" stroke-linejoin="round" id="strokes-fbbc58aa-1f9c-8016-8005-20cc88958414" class="strokes"><g class="stroke-shape"><path d="M3.594,7.229L12.939,15.771L22.284,7.229M12.939,7.229L12.939,7.229Z" style="fill: none; stroke-width: 2; stroke: rgb(255, 255, 255); stroke-opacity: 1;"/></g></g></g></g></g></svg>
@@ -52,18 +61,18 @@
         <div class="modal-menu-mobile">
             <ul class="anchor-list-mobile">
                 <li class="anchor-list-mobile__item">
-                    <a href="" class="anchor">Sobre mim</a>
+                    <a href="#about-me" class="anchor">Sobre mim</a>
                 </li>
                 <li class="anchor-list-mobile__item"> 
-                    <a href="" class="anchor">Cases</a>
+                    <a href="#my-cases" class="anchor">Cases</a>
                 </li>
                 <li class="anchor-list-mobile__item">
-                    <a href="" class="anchor">Serviços</a>
+                    <a href="#services" class="anchor">Serviços</a>
                 </li>
             </ul>
         </div><!-- Modal Menu Mobile -->
 
-        <section class="about-me">
+        <section id="about-me" class="about-me">
             <div class="line-content-wrapper">
                 <div class="line-content-wrapper__image">
                     <img src="assets/quinho-photo.webp" alt="">
@@ -94,7 +103,7 @@
             </div>
         </section><!-- About Me -->
 
-        <section class="my-cases">
+        <section id="my-cases" class="my-cases">
             <div class="container-my-cases">
                 <div class="title-wrapper">
                     <h2>Cases -</h2>
@@ -196,14 +205,43 @@
             <div class="line-banner-wrapper">
                 <img src="assets/code.webp" alt="Código de Programação" title="Código de Programação" class="img-banner">
             </div>
-        </section>
+        </section><!-- Banners -->
+
+        <section id="services" class="services">
+            <div class="left-container-controls">
+                <div class="title-wrapper">
+                    <h2>- Serviços</h2>
+                </div>
+                <div class="options-wrapper">
+                    <ul class="options-list">
+                        <li class="option-item active">
+                            <span>Ideação/Brainstorm</span>
+                            <svg width="46" xmlns="http://www.w3.org/2000/svg" height="46" id="screenshot-a27afb5f-9772-8011-8005-20e8075aa101" viewBox="0 0 46 46" xmlns:xlink="http://www.w3.org/1999/xlink" fill="none" version="1.1"><g id="shape-a27afb5f-9772-8011-8005-20e8075aa101" data-testid="idea-svgrepo-com" class="icon-lamp" width="200px" height="200px" rx="0" ry="0" style="fill: rgb(0, 0, 0);"><g id="shape-a27afb5f-9772-8011-8005-20e8076fcf94" data-testid="base-background" style="display: none;"><g class="fills" id="fills-a27afb5f-9772-8011-8005-20e8076fcf94"><rect width="46" height="43.125" x="0" transform="matrix(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000)" style="fill: none;" ry="0" fill="none" rx="0" y="2.875"/></g></g><g id="shape-a27afb5f-9772-8011-8005-20e80771f694" data-testid="SVGRepo_bgCarrier" rx="0" ry="0" style="fill: rgb(0, 0, 0);"/><g id="shape-a27afb5f-9772-8011-8005-20e8077225ec" data-testid="SVGRepo_tracerCarrier" rx="0" ry="0" style="fill: rgb(0, 0, 0);"/><g id="shape-a27afb5f-9772-8011-8005-20e8077225ed" data-testid="SVGRepo_iconCarrier" rx="0" ry="0" style="fill: rgb(0, 0, 0);"><g id="shape-a27afb5f-9772-8011-8005-20e807766f59" data-testid="svg-path"><g class="fills" id="fills-a27afb5f-9772-8011-8005-20e807766f59"><path d="M39.860,15.157C39.688,19.367,37.996,23.395,35.063,26.578C34.195,27.489,33.533,28.556,33.117,29.716L33.117,32.969C33.117,34.616,31.699,35.950,29.950,35.950L17.583,35.950C15.834,35.950,14.417,34.616,14.417,32.969L14.417,29.716C14.001,28.556,13.338,27.489,12.470,26.578C9.535,23.396,7.841,19.367,7.667,15.157C7.666,7.946,13.065,1.733,20.573,0.303C28.082,-1.128,35.621,2.620,38.600,9.264C39.440,11.130,39.871,13.137,39.860,15.157ZZM36.693,15.157C36.693,8.437,30.906,2.989,23.767,2.989C16.627,2.989,10.840,8.437,10.840,15.157C10.840,22.571,17.583,25.853,17.583,29.716L17.583,32.969L29.950,32.969L29.950,29.716C29.950,25.853,36.693,22.571,36.693,15.157ZZM31.533,39.449C31.534,39.844,31.368,40.224,31.071,40.505C30.774,40.785,30.371,40.942,29.950,40.942L17.583,40.942C16.709,40.942,16.000,40.275,16.000,39.452C16.000,38.629,16.709,37.961,17.583,37.961L29.950,37.961C30.823,37.961,31.531,38.627,31.533,39.449ZZM29.400,43.408C29.511,43.709,29.406,44.043,29.140,44.240C25.929,46.587,21.437,46.587,18.227,44.240C17.958,44.046,17.851,43.712,17.960,43.410C18.069,43.108,18.371,42.905,18.710,42.906L28.653,42.906C28.991,42.906,29.293,43.108,29.400,43.408ZZ" style="fill: rgb(85, 85, 85);"/></g></g></g></g></svg>
+                        </li>
+                        <li class="option-item">
+                            <span>Web Design</span>
+                            <svg width="46" xmlns="http://www.w3.org/2000/svg" height="46" id="screenshot-a27afb5f-9772-8011-8005-20e84c01ae0d" viewBox="0 0 46 46" xmlns:xlink="http://www.w3.org/1999/xlink" fill="none" version="1.1"><g id="shape-a27afb5f-9772-8011-8005-20e84c01ae0d" data-testid="pencil-svgrepo-com" version="1.1" width="800px" height="800px" rx="0" ry="0" style="fill: rgb(0, 0, 0);"><g id="shape-a27afb5f-9772-8011-8005-20e84c068567" data-testid="base-background" style="display: none;"><g class="fills" id="fills-a27afb5f-9772-8011-8005-20e84c068567"><rect width="45.905459147392094" height="45.938270449082665" x="0.04766082125945559" transform="matrix(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000)" style="fill: none;" ry="0" fill="none" rx="0" y="0.03014698998322274"/></g></g><g id="shape-a27afb5f-9772-8011-8005-20e84c09681a" data-testid="svg-path"><g class="fills" id="fills-a27afb5f-9772-8011-8005-20e84c09681a"><path d="M43.275,2.258C41.711,0.759,39.988,0.000,38.148,0.000C35.270,0.000,33.170,1.860,32.596,2.430C31.787,3.231,4.212,30.837,4.212,30.837C4.031,31.018,3.901,31.245,3.833,31.492C3.212,33.792,0.096,43.996,0.065,44.099C-0.096,44.622,0.048,45.192,0.432,45.577C0.709,45.852,1.078,46.000,1.455,46.000C1.604,46.000,1.753,45.977,1.900,45.930C2.004,45.895,12.530,42.491,14.263,41.973C14.491,41.904,14.700,41.781,14.869,41.613C15.964,40.530,41.682,15.079,43.390,13.315C45.156,11.495,46.033,9.597,45.999,7.680C45.963,5.787,45.046,3.964,43.275,2.258ZZM32.138,7.021C32.869,7.198,34.593,7.778,36.353,9.555C38.132,11.351,38.613,13.404,38.703,13.872C33.062,19.488,20.078,32.343,14.960,37.410C14.488,36.308,13.723,34.981,12.494,33.742C10.993,32.229,9.465,31.374,8.288,30.893C13.353,25.823,26.582,12.580,32.138,7.021ZZM6.341,33.315C7.130,33.525,8.767,34.134,10.435,35.816C11.721,37.114,12.327,38.544,12.600,39.416C10.606,40.058,6.239,41.566,3.466,42.461C4.288,39.754,5.677,35.539,6.341,33.315ZZM41.309,11.262C41.251,11.322,41.153,11.420,41.037,11.538C40.584,10.369,39.789,8.868,38.410,7.478C37.003,6.058,35.576,5.221,34.432,4.726C34.530,4.630,34.604,4.554,34.635,4.525C34.798,4.363,36.289,2.937,38.145,2.937C39.214,2.937,40.264,3.425,41.265,4.387C42.453,5.528,43.067,6.654,43.087,7.733C43.107,8.836,42.507,10.023,41.309,11.262ZZ" style="fill: rgb(85, 85, 85);"/></g></g></g></svg>
+                        </li>
+                        <li class="option-item">
+                            <span>Web Development</span>
+                            <svg width="46" xmlns="http://www.w3.org/2000/svg" height="46" id="screenshot-a27afb5f-9772-8011-8005-20e8ce8fa569" viewBox="0 0 46 46" xmlns:xlink="http://www.w3.org/1999/xlink" fill="none" version="1.1"><g id="shape-a27afb5f-9772-8011-8005-20e8ce8fa569" data-testid="code-svgrepo-com" width="800px" height="800px" rx="0" ry="0" style="fill: rgb(0, 0, 0);"><g id="shape-a27afb5f-9772-8011-8005-20e8ce93588c" data-testid="base-background" style="display: none;"><g class="fills" id="fills-a27afb5f-9772-8011-8005-20e8ce93588c"><rect width="46" height="46" x="0" transform="matrix(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000)" style="fill: none;" ry="0" fill="none" rx="0" y="0"/></g></g><g id="shape-a27afb5f-9772-8011-8005-20e8ce94e452" data-testid="svg-path"><g class="fills" id="fills-a27afb5f-9772-8011-8005-20e8ce94e452"><path d="M10.556,13.000L1.000,22.231L10.556,33.000M34.444,13.000L44.000,22.231L34.444,33.000M27.278,3.000L17.722,43.000" fill="none" stroke-linejoin="round" style="fill: none;"/></g><g fill="none" stroke-linejoin="round" id="strokes-a27afb5f-9772-8011-8005-20e8ce94e452" class="strokes"><g class="stroke-shape"><path d="M10.556,13.000L1.000,22.231L10.556,33.000M34.444,13.000L44.000,22.231L34.444,33.000M27.278,3.000L17.722,43.000" style="fill: none; stroke-width: 2; stroke: rgb(85, 85, 85); stroke-opacity: 1;"/></g></g></g></g></svg>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="right-container-textbox">
+                <div class="textbox-wrapper">
+                    <p class="paragraph-text">A fase de ideação é o primeiro passo do processo. Nessa fase, o objetivo é ter ideias e refiná-las de acordo com o objetivo proposto.</p>
+                </div>
+            </div>
+        </section><!-- Services -->
     </main>
 
     <footer>
         <div class="line-anchor-contacts-wrapper">
             <a href="https://www.linkedin.com/in/henrique-ferreira-pereira/" class="anchor-item" aria-label="Visitar Linkedin" target="_blank" rel="noreferrer nofollow noopener">Linkedin</a>
             <div class="divisor"></div>
-            <a href="/assets/cv-quinho.pdf" class="anchor-item" download="cv-quinho-pereira.pdf" aria-label="Download do Currículo">Download CV</a>
+            <a href="assets/cv-quinho.pdf" class="anchor-item" download="cv-quinho-pereira.pdf" aria-label="Download do Currículo">Download CV</a>
             <div class="divisor"></div>
             <a href="https://www.instagram.com/quinhopereira_" class="anchor-item" aria-label="Visitar Instagram" target="_blank" rel="noreferrer nofollow noopener">Instagram</a>
             <div class="divisor"></div>
--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,7 @@ const body = document.querySelector('body');
 
 $(document).ready(function(){
 
+
     // Dynamic Main Menu 
     const buttonTriggerMenu = $('#slide-down-menu');
     const menuWrapper = $('#menu-wrapper');
@@ -38,12 +39,30 @@ $(document).ready(function(){
     })
 
 
+    $('a[href^="#"]').on('click', function(e) {
+        e.preventDefault(); 
+
+        var target = $(this.getAttribute('href'));
+        let offset = 90;
+        
+        if(target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top - offset
+            }, 1000);
+        }
+
+        // Altera a URL sem recarregar a página
+        history.pushState(null, null, ' ');
+    });
+
+
     // Modal Cases
     const modalCases = $('.modal-my-cases');
     const btnOpenModal = $('button.btn-open-modal-cases');
     const btnCloseModal = $('button.close-modal-btn');
 
-    btnOpenModal.on('click', function(){
+    btnOpenModal.on('click', function(e){
+        e.preventDefault();
         modalCases.fadeIn();
         modalCases.css('display', 'flex');
         unloadScrollBars();
@@ -60,12 +79,38 @@ $(document).ready(function(){
             reloadScrollBars();
         }
     })
+
+
+
+    // Services Section
+    const services = document.querySelectorAll('.option-item');
+    const textBoxParagraph = document.querySelector('.paragraph-text');
+
+    services.forEach((service) =>{
+        service.addEventListener('click', function(){
+            
+            services.forEach((service) =>{
+                service.classList.remove('active');
+            })
+
+            this.classList.add('active');
+            if(service === services[0]){
+                textBoxParagraph.textContent = "A fase de ideação é o primeiro passo do processo. Nessa fase, o objetivo é ter ideias e refiná-las de acordo com o objetivo proposto."
+            }
+            if(service === services[1]){
+                textBoxParagraph.textContent = "Na fase de Web Design, são desenvolvidos protótipos de baixa e alta fidelidade, que serão refinados até se tornarem páginas e componentes com layouts intuitívos, acessíveis e com ótima experiência."
+            }
+            if(service === services[2]){
+                textBoxParagraph.textContent = "No processo de Web Development, o protótipo refinado  anteriormente é agora desenvolvido em código, passando por testes e melhorias até que o website tenha ótima experiência de usuário."
+            }
+        })
+    })
 })
 
 function handleSwipeMenuMobile(){
-    const offset = touchEndedY - touchStartedY;
-    console.log(offset)
-    if(touchEndedX < touchStartedX && offset < 30 && offset > -30){
+    const offsetY = touchEndedY - touchStartedY;
+    const offsetX = touchStartedX - touchEndedX;
+    if(touchEndedX < touchStartedX && offsetY < 30 && offsetY > -30 && offsetX > 100){
 
         menuMobileWrapper.classList.add('modal-visible');
 
